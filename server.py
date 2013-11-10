93d9641f1e126d649d42dd20f9f1dd23d2b21758
Ben Cohen <bn.chn2@gmail.com>
2013-11-09 20:17:46 -0500

    adding for zain

--- server.py
+++ server.py
@@ -33,7 +33,7 @@ app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 keymap = {}
 allLines = []
 g = []
-
+mac = sys.platform == "darwin"
 class Point(object):
 	'''Creates a point on a coordinate plane with values x and y.'''
 
@@ -48,17 +48,18 @@ def test():
 	return "Testing 123"
 @app.route('/', methods = ['GET','POST'])
 def main():
-	# print request.files
-	# try:
-	# 	r =  request.files['image']
-	# except:
-	# 	return "Failed to get image"
-	# r.save('postimage.JPG')
+	
+	try:
+		r =  request.files['image']
+		r.save('postimage.JPG')
+
+	except:
+		pass
 	retrain = False
 	if sys.platform != "darwin":
 		BGIMAGE = 'postimage.JPG'
 	else:
-		BGIMAGE = 'xo2.JPG'
+		BGIMAGE = 'xo3.JPG'
 
 	if retrain:
 		net = buildNetwork(4, 16, 2, bias=True)
@@ -69,9 +70,8 @@ def main():
 	
 	blue = simplecvimg.colorDistance((20,32,170)) * 1.5  #scale up
 
-	
 	blueBlobs = blue.findBlobs()
-
+	
 	# cv.WaitKey(10000)
 	# red = simplecvimg.colorDistance((62,5,13)) 
 	red = simplecvimg.colorDistance((130,20,20))
@@ -94,7 +94,8 @@ def main():
 
 	screen = second.crop().invert()
 	screen = screen.crop(screen.width/2, screen.height/2, screen.width-50, screen.height-20, centered=True)
-	#screen.show()
+	if mac:
+		screen.show()
 	screen.save("cropped.png")
 	w = screen.width * 1.0
 	h = screen.height * 1.0
@@ -102,19 +103,21 @@ def main():
 
 
 	if elements == None:
-		return jsonify{"Error": "No elements"}
-	circles = [x for x in elements if x.isCircle(tolerance=0.7)]
+		return jsonify({"Error": "No elements"})
+	circles = [x for x in elements if x.isCircle(tolerance=0.65)]
 	rectangles = [x for x in elements if x.isRectangle(tolerance=0.15)]
 		
 	circles = [x for x in circles if x not in rectangles]
 
 
 	for b in circles:
-		# b.show(color=(255,0,0))
+		if mac:
+			b.show(color=(255,0,0))
 		print "Coordinates: " + str(b.x/w) + ", " + str(b.y/h)
 		# cv.WaitKey(10)
 	for b in rectangles:
-		# b.show(color=(0,255,0))
+		if mac:
+			b.show(color=(0,255,0))
 		print "Coordinates: " + str(b.x/w) + ", " + str(b.y/h)
 		# cv.WaitKey(10)
 	centers = []
@@ -149,7 +152,8 @@ def main():
 			features = []
 			print b.width
 			i = b.blobImage().binarize()
-			# b.blobImage().show()
+			if mac:
+				b.blobImage().show()
 			i1 = raw_input()
 			if i1 == "0":
 				end = [0,1]
@@ -189,14 +193,16 @@ def main():
 		v = net.activate(features)
 		print v
 		if v[0] > v[1]:
-			# b.show(color=(0,0,255))
+			if mac:
+				b.show(color=(0,0,255))
 			if old[2] == 'rec':
 				class1.append(old[1])
 			else:
 				class2.append(old[1])
 			pass
 		else:
-			# b.show(color=(0,255,255))
+			if mac:
+				b.show(color=(0,255,255))
 			if old[2] == 'rec':
 				class3.append(old[1])
 			else:
@@ -227,25 +233,13 @@ def main():
 
 	for c in classes:
 		print len(c)
-	for x in class3[0:]:
-		x.show(color=(255,0,0))
-		cv.WaitKey(1000)
+
 	retValues = {'entities': {"class"+str(classes.index(c)) : c for c in classes}}
 	print retValues
 
 	while True:
 		cv.WaitKey(10)
-
 	# return jsonify(retValues)
-def seg_intersect(a,b):
-	a1 = Point(a[0],a[1])
-	a2 = Point(a[2],a[3])
-	b1 = Point(b[0],b[1])
-	b2 = Point(b[2],b[3])
-	return intersect(a1,a2,b1,b2)
-
-def ccw(A,B,C):
-    return (C.y-A.y) * (B.x-A.x) > (B.y-A.y) * (C.x-A.x)
 
 # Return true if line segments AB and CD intersect
 def intersect(A,B,C,D):
@@ -254,6 +248,6 @@ if __name__ == '__main__':
 	if sys.platform != "darwin":
 		app.run(host='0.0.0.0', debug=True, port=80)
 	else:
-		app.run(host='127.0.0.1',port=5000, debug=True)
-	# main()
+		#app.run(host='127.0.0.1',port=5000, debug=True)
+		main()
 
